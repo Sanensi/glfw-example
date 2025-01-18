5cb60b1c71daea119054d2eb322c2198e7f2797b
Sanensi <william.picard@hotmail.com>
2025-01-18 16:43:31 -0500

    Add project files.

---
+++ glfw-example.cpp
@@ -0,0 +1,12 @@
+﻿// glfw-example.cpp : Defines the entry point for the application.
+//
+
+#include "glfw-example.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
